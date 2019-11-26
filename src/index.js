b8de97b855ef68b0bbe73a3a5de0ea9f94d8a70d
Akshay Arora <aksarora4@publicisgroupe.net>
2019-11-26 12:45:28 +0530

    Added pagination component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,55 @@
 
-	import React from 'react'
-	import ReactDOM from 'react-dom'
-	import Example from './example'
-	import { DndProvider } from 'react-dnd'
-	import HTML5Backend from 'react-dnd-html5-backend'
-	import TouchBackend from 'react-dnd-touch-backend'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Example from './example'
+import { DndProvider } from 'react-dnd'
+import HTML5Backend from 'react-dnd-html5-backend'
+import TouchBackend from 'react-dnd-touch-backend'
+import Pagination from './components/Pagination'
+import * as Todos from './todos.json';
 
-	
-	function App() {
-		const backend = ("ontouchstart" in document.documentElement) ? TouchBackend : HTML5Backend;
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.backend = ("ontouchstart" in document.documentElement) ? TouchBackend : HTML5Backend;
+		this.state = {
+			list: Todos.default.filter(todo => todo.id <= 10),
+			pageLimit: 10
+		}
+	}
+
+	onPageChanged = (page) => {
+		const { currentPage, pageLimit } = page;
+		let start = (currentPage - 1) * pageLimit;
+		console.log(start)
+		this.setState({
+			list: Todos.default.filter(todo => todo.id > start && todo.id<=start+pageLimit)
+		})
+	}
+	render() {
+		const { list, pageLimit } = this.state;
 		return (
-			<div className="App">
-				<DndProvider backend={backend}>
-					<Example />
-				</DndProvider>
+			<div>
+				<div className="App">
+					<DndProvider backend={this.backend}>
+						<Example />
+					</DndProvider>
+				</div>
+				<div>
+					{
+						list.map(todo => <p key={todo.id}>{todo.id} - {todo.title}</p>)
+					}
+				</div>
+				<Pagination
+					totalRecords={Todos.default.length}
+					pageLimit={pageLimit}
+					pageNeighbours={3}
+					onPageChanged={this.onPageChanged}
+				/>
 			</div>
 		)
 	}
+}
 
-	const rootElement = document.getElementById('root')
-	ReactDOM.render(<App />, rootElement)
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
